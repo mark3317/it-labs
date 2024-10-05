8ce1f09e737778b0d71939f91f0471513dd480c6
Nokhrin_Mark <nokhrin@aqsi.ru>
2024-10-05 12:00:03 +0300

    build: имя модуля

--- itlabs/itlabsApp.swift
+++ itlabs/itlabsApp.swift
@@ -1,6 +1,6 @@
 //
-//  lab0App.swift
-//  lab0
+//  itlabsApp.swift
+//  itlabs
 //
 //  Created by Mark Nokhrin on 05.10.2024.
 //
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct lab0App: App {
+struct itlabsApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
