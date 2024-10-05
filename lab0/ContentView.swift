35cecd020d81965e088eda9691fb94207e05e9de
Nokhrin_Mark <nokhrin@aqsi.ru>
2024-10-05 10:48:59 +0300

    Initial Commit

---
+++ lab0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lab0
+//
+//  Created by Mark Nokhrin on 05.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lab0/lab0App.swift
@@ -0,0 +1,17 @@
+//
+//  lab0App.swift
+//  lab0
+//
+//  Created by Mark Nokhrin on 05.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct lab0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
