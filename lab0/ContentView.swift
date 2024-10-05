b3a58258405759ccb69680563be8d272ecf74620
Nokhrin_Mark <nokhrin@aqsi.ru>
2024-10-05 11:45:18 +0300

    feat: первые 20 чисел фибоначчи

--- lab0/ContentView.swift
+++ lab0/ContentView.swift
@@ -10,13 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(fibonacciSequence(count: 20), id: \.self) { number in
+                Text("\(number)")
+            }
         }
-        .padding()
     }
+}
+
+func fibonacciSequence(count: Int) -> [Int] {
+    guard count > 0 else { return [] }
+    var sequence = [0, 1]
+    while sequence.count < count {
+        let nextNumber = sequence[sequence.count - 1] + sequence[sequence.count - 2]
+        sequence.append(nextNumber)
+    }
+    return Array(sequence.prefix(count))
 }
 
 #Preview {
